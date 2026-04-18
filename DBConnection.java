748d700e953571e4a92b61b3a7ed01609eb916fb
hadia-asghar-x <1470353ha@gmail.com>
2026-04-18 20:48:23 +0500

    Library Managment System in Java

---
+++ DBConnection.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=library_db";
+    private static final String USER = "SOHAIB_LAPTOP"; 
+    private static final String PASSWORD = "1109005"; 
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
